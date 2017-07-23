22a9eb324b45a5b93c967eeaaf92c37d9db93a0e
Jiongheng Li <jionghengli@gmail.com>
2017-07-22 22:49:16 -0400

    Initial Commit

---
+++ GameTypes/ApocalypseMonsters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+
+namespace C3Mod.GameTypes
+{
+    internal class ApocalypseMonsters
+    {
+        public static List<int> Monsters = new List<int>();
+
+        public static void AddNPCs()
+        {
+            Monsters.Add(50);
+            Monsters.Add(154);
+            Monsters.Add(156);
+            Monsters.Add(179);
+            Monsters.Add(291);
+            Monsters.Add(292);
+            Monsters.Add(293);
+            Monsters.Add(222);
+            Monsters.Add(245);
+            Monsters.Add(289);
+            Monsters.Add(370);
+            Monsters.Add(395);
+            Monsters.Add(407);
+            Monsters.Add(419);
+            Monsters.Add(425);
+            Monsters.Add(454);
+            Monsters.Add(475);
+            Monsters.Add(480);
+            Monsters.Add(491);
+            Monsters.Add(521);
+            Monsters.Add(564);
+            Monsters.Add(576);
+        }
+    }
+}
